f3be143d2f59606a8645beedd13182f4f5298e8e
Rihhard Elm <elm.rihhard@gmail.com>
2023-07-28 10:42:12 +0300

    update urls

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -12,25 +12,25 @@ export default function Categories() {
             <div className="col">
                 <div className="row">
                 <button className="btn-remote" onClick={openProducts}>Remote Gear</button>
-                <a href="http://localhost:3000/products">
+                <a href="https://e-comm-client-eosin.vercel.app/products/">
                     
-                    <img src="http://localhost:3000/img/categories/itguy1.jpg" width={320} alt="itguy1"></img>
+                    <img src="img/categories/itguy1.jpg" width={320} alt="itguy1"></img>
                 </a>
                 </div>
                 <div className="row">
                 <button className="btn-office" onClick={openProducts}>Office Gear</button>
-                <a href="http://localhost:3000/products">
+                <a href="https://e-comm-client-eosin.vercel.app/products/">
                     
-                    <img src="http://localhost:3000/img/categories/itguy2.jpg" alt="itguy2"></img>
+                    <img src="img/categories/itguy2.jpg" alt="itguy2"></img>
                 </a>
                 </div>
             </div>
             <div className="col">
                 <div className="row">
                 <button className="btn-womens" onClick={openProducts}>Women</button>
-                <a href="http://localhost:3000/products">
+                <a href="https://e-comm-client-eosin.vercel.app/products/">
                     
-                    <img src="http://localhost:3000/img/categories/itgirl2.jpg" alt="itgirl2"></img>
+                    <img src="img/categories/itgirl2.jpg" alt="itgirl2"></img>
                 </a>
                 </div>
             </div>
@@ -39,17 +39,17 @@ export default function Categories() {
                     <div className="col">
                         <div className="row">
                         <button className="btn-sweats" onClick={openProducts}>Sweats</button>
-                        <a href="http://localhost:3000/products">
+                        <a href="https://e-comm-client-eosin.vercel.app/products/">
                             
-                            <img src="http://localhost:3000/img/categories/sweats.jpg" width={506} alt="sweats"></img>
+                            <img src="img/categories/sweats.jpg" width={506} alt="sweats"></img>
                         </a>
                         </div>
                     </div>
                  </div>
                  <div className="row">
                     <button className="btn-accessories" onClick={openProducts}>Accessories</button>
-                    <a href="http://localhost:3000/products">
-                    <img src="http://localhost:3000/img/categories/accessories.jpg" height={325} width={506} alt="accessories"></img>
+                    <a href="https://e-comm-client-eosin.vercel.app/products/">
+                    <img src="img/categories/accessories.jpg" height={325} width={506} alt="accessories"></img>
                     </a>
                  </div>
             </div>
